a0250cd915eda05cfee850b82d1208e935f36a3f
McKayla Kleinschrodt <gh1880@wayne.edu>
2022-03-05 14:47:21 -0500

    Update frontEnd.py

--- frontEnd.py
+++ frontEnd.py
@@ -1,63 +1,83 @@
 from tkinter import *
-# from tkinter import ttk
+from tkinter import ttk
+
 
 root = Tk()
 root.geometry("500x500")
 
-class frontEnd():
-    fullNameLabel = Label(root, text = "Full Name: ")
+
+tabControl = ttk.Notebook(root)
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+tabControl.add(tab1, text ='Add New Employee')
+Label(tab1)
+tabControl.add(tab2, text ='Search Employee Database')
+Label(tab2)
+tabControl.pack(expand = 1, fill ="both")
+
+
+class addEmployee():
+
+    fullNameLabel = Label(tab1, text = "Full Name: ")
     fullNameLabel.pack()
-    fullName = Entry(root, width= 40)
+    fullName = Entry(tab1, width= 40)
     fullName.focus_get()
     fullName.pack()
 
-    positionLabel = Label(root, text = "Position: ")
+    positionLabel = Label(tab1, text = "Position: ")
     positionLabel.pack()
-    position = Entry(root, width= 40)
+    position = Entry(tab1, width= 40)
     position.focus_get()
     position.pack()
     
-    ssnLabel = Label(root, text = "SSN: ")
+    ssnLabel = Label(tab1, text = "SSN: ")
     ssnLabel.pack()
-    ssn = Entry(root, width= 40)
+    ssn = Entry(tab1, width= 40)
     ssn.focus_get()
     ssn.pack()
 
-    addressLabel = Label(root, text = "Home Address: ")
+    addressLabel = Label(tab1, text = "Home Address: ")
     addressLabel.pack()
-    address = Entry(root, width= 40)
+    address = Entry(tab1, width= 40)
     address.focus_get()
     address.pack()
 
-    emailAddLabel = Label(root, text = "Email: ")
+    emailAddLabel = Label(tab1, text = "Email: ")
     emailAddLabel.pack()
-    emailAdd = Entry(root, width= 40)
+    emailAdd = Entry(tab1, width= 40)
     emailAdd.focus_get()
     emailAdd.pack()
 
-    phoneNumberLabel = Label(root, text = "Phone Number: ")
+    phoneNumberLabel = Label(tab1, text = "Phone Number: ")
     phoneNumberLabel.pack()
-    phoneNumber = Entry(root, width= 40)
+    phoneNumber = Entry(tab1, width= 40)
     phoneNumber.focus_get()
     phoneNumber.pack()
 
-    skillsLabel = Label(root, text = "Skills: ")
+    skillsLabel = Label(tab1, text = "Skills: ")
     skillsLabel.pack()
-    skills = Entry(root, width= 40)
+    skills = Entry(tab1, width= 40)
     skills.focus_get()
     skills.pack()
 
-    empIDLabel = Label(root, text = "Employee ID: ")
+    empIDLabel = Label(tab1, text = "Employee ID: ")
     empIDLabel.pack()
-    employeeID = Entry(root, width= 40)
+    employeeID = Entry(tab1, width= 40)
+    employeeID.focus_get()
+    employeeID.pack()
+
+class searchEmpID():
+    empIDLabel = Label(tab2, text = "Employee ID: ")
+    empIDLabel.pack()
+    employeeID = Entry(tab2, width= 40)
     employeeID.focus_get()
     employeeID.pack()
 
 
-newEmployeeButtonSet = Button(root, height = 1, width = 20, text = "Enter New Employee", command = lambda: frontEnd())         
+newEmployeeButtonSet = Button(tab1, height = 1, width = 20, text = "Enter New Employee", command = lambda: addEmployee())         
 newEmployeeButtonSet.pack()
 
-searchEmployeeDBSet = Button(root, height = 1, width = 20, text = "Search for Employee", command = lambda: frontEnd()) 
+searchEmployeeDBSet = Button(tab2, height = 1, width = 20, text = "Search for Employee", command = lambda: searchEmpID()) 
 searchEmployeeDBSet.pack()
 
 root.mainloop()
